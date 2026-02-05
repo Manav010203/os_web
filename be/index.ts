7fbb07747bcbd0235b6d3bcf875de15bebce7f81
Manav010203 <manav01022003@gmail.com>
2026-02-05 22:16:58 +0530

    added

--- be/index.ts
+++ be/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/auth/signup",(req,res)=>{
+    
+})
+export default app;
---
+++ be/server.ts
@@ -0,0 +1,12 @@
+
+import app from ".";
+
+
+
+const port = 3000;
+
+
+
+app.listen(port,()=>{
+    console.log(`listening on ${port}`);
+})
