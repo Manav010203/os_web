66de39b15e45d89ae2b5270b73d318e2738901a7
Manav010203 <manav01022003@gmail.com>
2026-02-07 18:21:57 +0530

    up

--- be/index.ts
+++ be/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import { middleware } from "./middleware";
 
 const app = express();
 
 app.use(express.json());
 
 app.post("/auth/signup",(req,res)=>{
-    
+
+})
+app.post("/auth/signin",(req,res)=>{
+
+})
+app.post("projects",middleware,async(req,res)=>{
+
 })
 export default app;
